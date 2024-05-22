8d7ab08640310d830e37dad34416a122f07a79e8
Anyamanee <unyamanee1a@gmail.com>
2024-05-22 21:48:55 +0700

    Add Container

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TextColor text="Hello World" color="red" />
-        <TextColor text="สวัสดีชาวโลก" color="blue" />
-        <TextColor text="อัญมณี" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">hello</div>
     </div>
   );
 }
-
-const TextColor = ({
-  text,
-  color = "black",
-}: {
-  text: string;
-  color?: string;
-}) => {
-  return <p style={{ color }}>{text.toUpperCase()}</p>;
-};
 
 export default App;
